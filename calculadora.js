9268840323cdf72c2701ee11d81b360358509907
ana <anacarolinarangel16@gmail.com>
2023-08-29 15:33:16 -0300

    calc ana

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,10 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multiplicar = require("./modulos/multiplicar");
+var subtrair = require("./modulos/subtrair");
 
-console.log(somar(20,90));
+
+console.log("A soma de X e Y é: "+somar(20,90));
 console.log(media(20,90));
+console.log(multiplicar(20,90));
+console.log(subtrair(20,90));
